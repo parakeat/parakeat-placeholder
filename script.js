9c233236034cc638963811bc2fbe506715380a52
sam hinton <sam.hinton88@gmail.com>
2018-05-11 11:56:13 +0100

    cleaned up comments

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+'use strict'
+
 document.getElementById('soon').addEventListener('mouseover', function() {
-  console.log('inside addEventListener')
   var logo = document.getElementsByClassName('parakeat-logo')[0];
   logo.classList.remove('fade-in')
   logo.classList.add('bouncer');
-})
+});
+
+
 
