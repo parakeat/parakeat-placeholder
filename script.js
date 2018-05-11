2caba170e753c9426907b97257a42529fdaa3357
sam hinton <sam.hinton88@gmail.com>
2018-05-11 18:42:38 +0100

    removed console.log from script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
-console.log('top')
+
 document.getElementById('tribe').addEventListener('click', function() {
-  console.log('click')
+
   var logo = document.getElementsByClassName('parakeat-logo')[0];
   logo.classList.remove('fade-in')
   logo.classList.add('bouncer');
