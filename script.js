fd22c41fd3216a74331a7addb9c71d11d1c77c3b
sam hinton <sam.hinton88@gmail.com>
2018-05-11 18:15:29 +0100

    changed cname to parakeat.co.uk, changed event listener to onclick

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-document.getElementById('soon').addEventListener('mouseover', function() {
+document.getElementById('tribe').addEventListener('onclick', function() {
   var logo = document.getElementsByClassName('parakeat-logo')[0];
   logo.classList.remove('fade-in')
   logo.classList.add('bouncer');
