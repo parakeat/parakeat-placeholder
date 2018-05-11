fb8ee586928f640a11543dc866c8a31de950f85a
sam hinton <sam.hinton88@gmail.com>
2018-05-11 18:40:51 +0100

    trigger animation on click

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 'use strict'
-
-document.getElementById('tribe').addEventListener('onclick', function() {
+console.log('top')
+document.getElementById('tribe').addEventListener('click', function() {
+  console.log('click')
   var logo = document.getElementsByClassName('parakeat-logo')[0];
   logo.classList.remove('fade-in')
   logo.classList.add('bouncer');
+  setTimeout(function() {
+    logo.classList.remove('bouncer');
+
+  }, 3000);
 });
 
 
